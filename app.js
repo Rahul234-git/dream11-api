408af1a97d154cff51e6d4c365e434c381b27198
Rahul234-git <72328981+Rahul234-git@users.noreply.github.com>
2023-06-03 23:41:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin","*");
     res.setHeader("Access-Control-Allow-Methods","GET,POST");
     res.setHeader("Access-Control-Allow-Headers","Content-Type,Authorization");
+    res.setHeader("Access-Control-Allow-Credentials","true")
     next();
 })
 app.use('/api',router);
